4c94638ae2c545579c833451385a99bced5d17b1
Vinhuanc <huann.chenn@gmail.com>
2022-11-17 16:06:26 -0600

    Initial Commit

---
+++ Assignment12/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment12
+//
+//  Created by Chen, Huan on 11/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
